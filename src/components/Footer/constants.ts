7698cd4c944a808b22efa734223296eaec118fa8
Juan Diego Carballo <juandiegocarballo2701@gmail.com>
2025-01-30 01:24:29 -0600

    Add Footer component and company information constants

---
+++ src/components/Footer/constants.ts
@@ -0,0 +1,9 @@
+export const COMPANY_INFO = {
+    name: "WARDWORK",
+    description:
+        "A decentralized freelance platform leveraging blockchain technology for secure and efficient collaboration.",
+    version: "v1.0.0",
+    requiredNodeVersion: "v23.3.0",
+    requiredPnpmVersion: "v9.15.2",
+    githubRepo: "https://github.com/WARDWORK/wardwork",
+}
