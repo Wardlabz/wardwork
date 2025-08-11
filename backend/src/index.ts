ccabd419fd46cff07e02980156a232bce5dddcf3
Solomon Emmanuel <soulex04292003@gmail.com>
2025-08-11 12:42:30 +0100

    refactor: make all custom error classes extend AppError to unify error structure and improve HTTP response consistency

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,7 @@ import nftRoutes from "@/routes/nft.routes";
 import contractRoutes from "@/routes/contract.routes";
 import projectRoutes from '@/routes/project.routes';
 import userRoutes from '@/routes/user.routes';
+import { ErrorHandler } from "./utils/AppError";
 
 
 const app = express();
@@ -34,7 +35,7 @@ app.use('/api/users', userRoutes);
 app.get("/", (_req, res) => {
   res.send("💼 WARDWORK backend is up and running!");
 });
-
+app.use(ErrorHandler);
 app.listen(port, () => {
   console.log(`🚀 WARDWORK server is live at http://localhost:${port}`);
   console.log("🌐 Connecting freelancers and clients around the world...");
